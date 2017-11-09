569a2f0728f5af88e41f3c013137122c9052a2bc
Greg Lownes <greg@lownes.net>
2017-11-09 08:05:33 -0500

    Add boostrap CSS and JS, and also jQuery On branch bootstrap Changes to be committed: 	modified:   index.html 	modified:   main.js 	modified:   package-lock.json 	modified:   package.json 	modified:   renderer.js

--- renderer.js
+++ renderer.js
@@ -1,4 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-console.log('inside renderer.js');
+
+let count = 0
+$('#click-counter').text(count.toString())
+$('#countbtn').on('click', () => {
+   count ++
+   $('#click-counter').text(count)
+})
+
+$('#myTab a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTab a:first').tab('show') // Select first tab
