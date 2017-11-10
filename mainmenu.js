7af32ced8332454f1eeac770e07f70f25db7e743
Greg Lownes <greg@lownes.net>
2017-11-10 09:20:01 -0500

    Add left nav. Built menu template in mainmenu.js. SPA design - breakout each page to its own section: see sections folder - renderer.js calls menu.js - menu.js handles navigation from left nav and menu - page specific js moved to menu.js (which runs after page sections are loaded) For each new section - create file in sections folder - add import to index.html <head> - add left nav anchor in index.html - add menu item in mainmenu.js
    On branch menus
    Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	new file:   mainmenu.js
    	new file:   menu.js
    	modified:   renderer.js
    	new file:   sections/bstable.html
    	new file:   sections/bstabnav.html
    	new file:   sections/jqcounter.html
    	new file:   sections/menu.html
    	new file:   sections/versions.html

---
+++ mainmenu.js
@@ -0,0 +1,124 @@
+const {app, Menu} = require('electron')
+
+const template = [
+  {
+    label: 'Demo',
+    submenu: [
+      {
+        label: 'Versions',
+        click (menuItem, browserWindow, event) {
+          browserWindow.webContents.send('menucall', 'versions');
+         }
+      },
+      {
+        label: 'jQuery Counter',
+        click (menuItem, browserWindow, event) {
+          browserWindow.webContents.send('menucall', 'jqcounter');
+         }
+      },
+      {
+        label: 'Bootstrap Table with Hover Rows',
+        click (menuItem, browserWindow, event) {
+          browserWindow.webContents.send('menucall', 'bstable');
+         }
+      },
+      {
+        label: 'Bootstrap Tab Nav',
+        click (menuItem, browserWindow, event) {
+          browserWindow.webContents.send('menucall', 'bstabnav');
+         }
+      },
+      {
+        label: 'Custom Menus',
+        click (menuItem, browserWindow, event) {
+          browserWindow.webContents.send('menucall', 'menu');
+         }
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://electron.atom.io') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  template[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  template[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
